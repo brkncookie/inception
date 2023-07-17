d160c7f23004d15c4ac0b10050d7f6c949b86d0e
mnadir <mnadir@student.1337.ma>
2023-07-17 10:53:39 +0100

    gotta get nginx done

--- srcs/requirements/wordpress/tools/wpsetup.sh
+++ srcs/requirements/wordpress/tools/wpsetup.sh
@@ -11,6 +11,6 @@ wp core install --allow-root --url=${DOMAIN_NAME} --title=${WP_TITLE} --admin_us
 	--admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --skip-email
 wp user create --allow-root $WP_USER $WP_EMAIL --user_pass=${WP_USER_PASSWORD} --role=author
 
-
+sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf && mkdir /run/php
 
 exec php-fpm7.3 -F
