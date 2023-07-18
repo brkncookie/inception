fd3132c7f9e0e84eb3fab90c98265935174682e6
brkncookie <the0007end@gmail.com>
2023-07-18 19:04:57 +0100

    gotta connect wp and the db :

--- srcs/requirements/mariadb/tools/dbsetup.sh
+++ srcs/requirements/mariadb/tools/dbsetup.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-sed -i 's/^bind-address\s*=.*/bind-address = 0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.conf
+sed -i 's/^bind-address\s*=.*/bind-address = 0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
 
 service mariadb start
 
 echo "create database if not exists ${DB_NAME} ;" >> /tmp/db.sql
-echo "create user if not exists ${DB_USER}@'%' identified by ${DB_USER_PASSWORD} ;" >> /tmp/db.sql
-echo "grant all privileges on ${DB_NAME}.* to ${DB_USER}@'%' ;" >> /tmp/db.sql
+echo "create user if not exists '${DB_USER}'@'%' identified by '${DB_USER_PASSWORD}' ;" >> /tmp/db.sql
+echo "grant all privileges on ${DB_NAME}.* to '${DB_USER}'@'%' ;" >> /tmp/db.sql
 echo "flush privileges ;" >> /tmp/db.sql
 
 mariadb < /tmp/db.sql
 
-service mariadb restart 
-service mariadb stop 
+kill `pidof mariadbd`
 
 exec mariadbd
--- srcs/requirements/wordpress/tools/wpsetup.sh
+++ srcs/requirements/wordpress/tools/wpsetup.sh
@@ -2,15 +2,15 @@
 
 mkdir -p /var/www/html && cd /var/www/html
 
-curl -o /usr/local/bin/wp ttps://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar \
-	&& chmod +x /usr/local/bin/wp 
+curl -o /usr/local/bin/wp https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar \
+	&& chmod +x /usr/local/bin/wp
 
 wp core download --allow-root
-wp config set --allow-root --dbhost=${DB_HOST} --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD}
+wp core config --allow-root --dbhost=${DB_HOST} --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_USER_PASSWORD}
 wp core install --allow-root --url=${DOMAIN_NAME} --title=${WP_TITLE} --admin_user=${WP_ADMIN} \
 	--admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --skip-email
-wp user create --allow-root $WP_USER $WP_EMAIL --user_pass=${WP_USER_PASSWORD} --role=author
+wp user create --allow-root ${WP_USER} ${WP_EMAIL} --user_pass=${WP_USER_PASSWORD} --role=author
 
-sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf && mkdir /run/php
+sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/g' /etc/php/7.4/fpm/pool.d/www.conf && mkdir /run/php
 
-exec php-fpm7.3 -F
+exec php-fpm7.4 -F
